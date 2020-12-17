80fbc674bc72e3b84d97d46c703ddfa8bcf78c95
taemma <74910643+taemma@users.noreply.github.com>
2020-12-17 13:59:50 +0200

    Add files via upload

---
+++ Lukko.php
@@ -0,0 +1,30 @@
+<?php
+
+        class Lukko extends CI_Model {
+
+              /*  function __construct(){
+
+                parent::__construct();
+                $this->load->database();
+
+                }*/
+
+                function lukontila(){
+                        $this->load->database();
+
+                        $query = $this->db->query("SELECT * FROM logi ORDER BY id DESC");
+
+                        $query->result_array();
+                        return $query->result_array();
+                }
+                function atmtila(){
+/*foreach*/
+                        $query = $this->db->query('SELECT DISTINCT id,msg,aika  FROM lukontila ORDER BY id DESC LIMIT 1');
+
+                        $query->result_array();
+                        return $query->result_array();
+                        /*$this->db->replace('lukontila');*/
+                }
+
+        }
+?>
